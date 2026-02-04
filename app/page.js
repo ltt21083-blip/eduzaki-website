06fa5377116a5f2b66d7124df4567c9796ff6d15
ltt21083-blip <ltt21083@gmail.com>
2026-02-04 14:38:25 +0530

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "Arial", padding: "40px" }}>
+      <h1>EduZaki</h1>
+      <p>Personalized online tuition for smart learning.</p>
+      <p>Website is successfully connected 🎉</p>
+    </main>
+  );
+    }
